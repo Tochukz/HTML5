ea3f0a5c5ebbd48d3db29fed361eca9cbb07af94
Tochukwu Nwachukwu <tnwachukwu@realmdigital.co.za>
2019-09-27 05:14:04 +0200

    Webworkers

---
+++ WebWorker/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Geoogle Geolocation</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" type="text/css" href="http://localhost/local-cdn/bootstrap-3.3.4-dist/css/bootstrap.min.css">
+    <!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"> </script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"> </script>	
+	<![endif]-->
+</head>
+
+<body>
+    <nav class="navbar navbar-default navbar-static-top" id="nav">
+        <div class="container">
+            <div class="navbar-header">
+                <a href="index.php" class="navbar-brand">HTML5 Geolocation</a>
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-menu" aria-expanded="false">
+                <span class="sr-only"> Toggle Navigation </span>
+                <span class="icon-bar"> </span>
+                <span class="icon-bar"> </span>
+                <span class="icon-bar"> </span>
+            </button>
+            </div>
+            <div class="collapse navbar-collapse navbar-right" id="nav-menu">
+                <ul class="nav navbar-nav">
+                    <li><a href="index.php">HOME</a></li>
+                    <li><a href="aboutus.php">ABOUT US</a></li>
+                    <li><a href="services.php">SERVICES</a></li>
+                    <li><a href="webdesign.php">WEB DESIGN</a></li>
+                    <li><a href="portfolio.php">PORTFOLIO</a></li>
+                    <li><a href="contactus.php">CONTACT US</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!--<script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>-->
+    <script src="http://localhost/local-cdn/jquery/jquery-1.11.3.min.js"></script>
+    <script src="js/webwork.js"></script>
+    <!--
+    <script id="worker1" type="javascript/worker">
+        self.onmessage = function(event){ var jsonText = event.data; var jsonData = JSON.parse(jsonText); self.postMessage(jsonData); }
+    </script>
+    -->
+</body>
+
+</html>
