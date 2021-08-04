b02e45a19b8b956dce69a848e6982e3b53751cf0
Tochukwu Nwachukwu <truetochukz@gmail.com>
2021-08-04 21:17:46 +0200

    Web Push Notification

--- WebPush/public/js/index.js
+++ WebPush/public/js/index.js
@@ -1,9 +1,9 @@
 const publicVapidKey='BGy_8YCk-2GZew1NGx47BWsrp9eeT_xHynU-uABE5FklAi1SgocFQXTefYG2eNrrCoTQVBz90krRmOJlNLZH45g';
 
-if ('serviceWorker' in navigator) {    
-    run(); 
+if ('serviceWorker' in navigator) {
+    run();
 } else {
-    console.error('Service Wokrer API not supported: ', navigator);
+    console.error('Service Worker API not supported: ', navigator);
 }
 
 let subscription;
@@ -11,9 +11,9 @@ async function run() {
     try {
         // Registering service worker
         const registration = await navigator.serviceWorker.register('/service-worker.js', {scope: '/'});
-    
+
         // Subscribe to push notification
-        subscription = await registration.pushManager.subscribe({        
+        subscription = await registration.pushManager.subscribe({
             userVisibleOnly: true,
             applicationServerKey: urlBase64ToUint8Array(publicVapidKey),
         });
@@ -30,10 +30,10 @@ async function run() {
     }
 }
 
-function sendNote() {   
-    const message = document.querySelector('#message').value;  
-    const payload = {subscription, data: { message }}; 
- 
+function sendNote() {
+    const message = document.querySelector('#message').value;
+    const payload = {subscription, data: { message }};
+
     fetch('/sendNotification', {
         method: 'POST',
         body: JSON.stringify(payload),
@@ -57,13 +57,12 @@ function unRegister() {
 function urlBase64ToUint8Array(base64String) {
     const padding = '='.repeat((4 - base64String.length % 4) % 4);
     const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
-   
+
     const rawData = window.atob(base64);
     const outputArray = new Uint8Array(rawData.length);
-   
+
     for (let i = 0; i < rawData.length; ++i) {
       outputArray[i] = rawData.charCodeAt(i);
     }
     return outputArray;
 }
-   
